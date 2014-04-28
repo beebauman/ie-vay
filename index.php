aca27d553199f526a7666038c6401b48f4effeff
root <root@beebauman.com>
2014-04-28 20:08:02 +0000

    Initial commit.

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Unsupported Browser</title>
+		<meta http-equiv="imagetoolbar" content="no" />
+		<style type="text/css">
+      body { text-align: center; font-family: Arial, sans-serif; }
+      div { max-width:900px; margin:0 auto; }
+      h1 { font-size: 24px; font-weight: 900; }
+      h2 { font-size: 18px; font-weight: 600; }
+      h3 { font-size: 36px; font-weight: 900; }
+      table { margin:0 auto; }
+      a img { border: none; width: 250px; }
+      p { font-size: 14px; font-weight: 600; }
+		</style>
+	</head>
+	<body>
+	  <div>
+  		<h1>Your web browser, Internet Explorer 6 or 7, is not supported by this site. It is incompatible with modern web standards, and vulnerable to serious virus exploits.</h1>
+  		<h3>Install a good browser now.</h3>
+  		<table width="600" border="0" cellpadding="10">
+        <tr>
+          <td><a href="http://www.google.com/chrome" title="Download Google Chrome"><img src="chrome.png" alt="Google Chrome" /></a><p>Google Chrome</p></td>
+          <td><a href="http://www.apple.com/safari/" title="Download Apple Safari"><img src="safari.png" alt="Apple Safari" /></a><p>Apple Safari</p></td>
+          <td><a href="http://getfirefox.com/" title="Download Mozilla Firefox"><img src="firefox.png" alt="Mozilla Firefox" /></a><p>Mozilla Firefox</p></td>
+        </tr>
+      </table>
+	  </div>
+	</body>
+</html>
